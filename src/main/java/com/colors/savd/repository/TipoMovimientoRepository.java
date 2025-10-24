87f0ee3fdc910c85f5ef1e23afdd071d3f7d47a0
SpiritD98 <danmespiritu_98@hotmail.com>
2025-10-24 11:14:13 -0500

    Se agrego los repositorios

--- src/main/java/com/colors/savd/repository/TipoMovimientoRepository.java
+++ src/main/java/com/colors/savd/repository/TipoMovimientoRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface TipoMovimientoRepository extends JpaRepository<TipoMovimiento, Long> {
-  Optional<TipoMovimiento> findByCodigo(String codigo);}
+  Optional<TipoMovimiento> findByCodigo(String codigo);
+}
