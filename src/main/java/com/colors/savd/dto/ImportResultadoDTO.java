92e1e652d1728653d0c24d8cf53c603465e3f093
Lfabiann <lfabian.trodriguez@gmail.com>
2025-10-24 12:48:05 -0500

    Update ImportResultadoDTO.java

--- src/main/java/com/colors/savd/dto/ImportResultadoDTO.java
+++ src/main/java/com/colors/savd/dto/ImportResultadoDTO.java
@@ -1,5 +1,16 @@
 package com.colors.savd.dto;
 
-public class ImportResultadoDTO {
+import lombok.*;
 
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImportResultadoDTO {
+  private Long bitacoraId;
+  private Integer filasOk;
+  private Integer filasError;
+  private List<String> erroresMuestra; // primeros N errores para mostrar en UI
 }
