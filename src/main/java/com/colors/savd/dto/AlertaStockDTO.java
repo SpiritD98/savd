0386ba6b471f6114ec6c98f20bc2d35711d5e84b
Lfabiann <lfabian.trodriguez@gmail.com>
2025-10-24 12:47:24 -0500

    Update AlertaStockDTO.java

--- src/main/java/com/colors/savd/dto/AlertaStockDTO.java
+++ src/main/java/com/colors/savd/dto/AlertaStockDTO.java
@@ -1,5 +1,17 @@
 package com.colors.savd.dto;
 
-public class AlertaStockDTO {
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AlertaStockDTO {
+  private Long skuId;
+  private String sku;
+  private Integer stockActual;
+  private Integer minStock;
+  private Integer coberturaDias; // días estimados de cobertura
+  private Integer rop;           // Reorder Point (punto de pedido)
+  private String semaforo;       // ROJO/AMARILLO/VERDE
 }
