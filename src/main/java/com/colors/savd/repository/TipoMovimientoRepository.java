380610f996c98065d6681bd89b3bbeb07b74f5b4
José Tafur <josemtg1234@gmail.com>
2025-10-24 10:48:37 -0500

    Create TipoMovimientoRepository.java

---
+++ src/main/java/com/colors/savd/repository/TipoMovimientoRepository.java
@@ -0,0 +1,12 @@
+package com.colors.savd.repository;
+
+import com.colors.savd.model.TipoMovimiento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TipoMovimientoRepository extends JpaRepository<TipoMovimiento, Long> {
+  Optional<TipoMovimiento> findByCodigo(String codigo);
+}
